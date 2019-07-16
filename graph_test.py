c5a91232e54ddf0c759819bee0f3045d185a0ef0
Egon Fiedler <egonfiedler@Egons-MacBook-Air.local>
2019-07-16 02:21:16 -0700

    Missing almost everything so far

--- graph_test.py
+++ graph_test.py
@@ -11,7 +11,16 @@ class TestVertex(unittest.TestCase):
         # assert vertex.get_neighbors() == {}
 
     def test_add_neighbor(self):
-        
+        egon_vertex = Vertex('Egon')
+        yurac_vertex = Vertex('Yurac') 
+        # Make to check the main values in both Vertex
+        print("Egon Vertex: ", egon_vertex.id)
+        print(egon_vertex.add_neighbor(yurac_vertex))
+        assert egon_vertex.add_neighbor(yurac_vertex)
+        assert egon_vertex.neighbors == {yurac_vertex: 'yurac_vertex'}
+
+    # def test_more_complex_add_neighbor(self):
+
 
 if __name__ == '__main__':
     unittest.main()
